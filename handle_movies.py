16f378e63114d1a5193167f752bd154c0f1e0e71
David Hromyk <38985997+DavidHromyk@users.noreply.github.com>
2020-04-09 18:47:50 -0700

    Update handle_movies.py

--- handle_movies.py
+++ handle_movies.py
@@ -1,7 +1,7 @@
-import os
+import os #imports os (file) module
 print ('Starting movie transfer')
-os.chdir('C:/Users/David/Desktop')
-from copy_movies import *
+os.chdir('C:/Users/David/Desktop') # change to my Desktop dir
+from copy_movies import * #imports from my other code file
 
-extension.mp4()
-extension.mkv()
+extension.mp4() #calls the class extension with the mp4 function
+extension.mkv() #calls the class extension with the mkv function
