2f2a547ce216777aa7588a825f5d2ff4f78d337b
David Hromyk <38985997+DavidHromyk@users.noreply.github.com>
2020-04-09 18:50:36 -0700

    Update copy_movies.py

--- copy_movies.py
+++ copy_movies.py
@@ -1,8 +1,8 @@
-import os
-import shutil
+import os # calls os module to handle files and directories
+import shutil # calls shutil module to handle copying files
 
 
-def copy_files(directory, ext):
+def copy_files(directory, ext): # function to check if file ends with ext and copies over to external drive if it does
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(ext):
@@ -12,7 +12,7 @@ def copy_files(directory, ext):
         return ('No movie files')
 
 
-def check_files(directory):
+def check_files(directory): # checks if the files made it to the external hard drive
     for root, dirs, files in os.walk(directory):
         for file in files:
             if os.path.exists(f'{directory}/{file}'):
@@ -21,23 +21,23 @@ def check_files(directory):
         return False
 
 
-def remove_files(directory, ext):
+def remove_files(directory, ext): # removes the file after it is copied over
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(ext):
                 os.remove(os.path.join(root, file))
 
 
-class Extension():
+class Extension(): # class for handling different extension types
 
-    def mp4(self):
+    def mp4(self): # handles mp4 files
         copy_files('C:/Users/David/Downloads', '.mp4')
         check = check_files('D:/Temp')
         if check == True:
             remove_files('C:/Users/David/Downloads/', '.mp4')
             return ('Copied mp4 files')
 
-    def mkv(self):
+    def mkv(self): # handles mkv files 
         copy_files('C:/Users/David/Downloads', '.mkv')
         check = check_files('D:/Temp')
         if check == True:
@@ -45,6 +45,6 @@ class Extension():
             return ('Copied mkv files')
 
 
-extension = Extension()
+extension = Extension() # calls the class 
 extension.mkv()
 extension.mp4()
