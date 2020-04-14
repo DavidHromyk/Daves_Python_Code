8d9d0f613b39f9f41971c4f13f17aabccf9513d5
David Hromyk <38985997+DavidHromyk@users.noreply.github.com>
2020-04-14 15:29:15 -0700

    Update copy_movies.py

--- copy_movies.py
+++ copy_movies.py
@@ -2,24 +2,22 @@ import os # calls os module to handle files and directories
 import shutil # calls shutil module to handle copying files
 
 
-def copy_files(directory, ext): # function to check if file ends with ext and copies over to external drive if it does
+# function to check if file ends with ext and copies over to external drive if it does
+def copy_files(directory, ext):
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(ext):
                 shutil.copy2(os.path.join(root, file), f'D:/Temp/{file}')
                 print(os.path.join(root, file))
-    else:
-        return ('No movie files')
 
+            else:
+                print('No movie files')
 
 def check_files(directory): # checks if the files made it to the external hard drive
     for root, dirs, files in os.walk(directory):
         for file in files:
             if os.path.exists(f'{directory}/{file}'):
                 return True
-    else:
-        return False
-
 
 def remove_files(directory, ext): # removes the file after it is copied over
     for root, dirs, files in os.walk(directory):
@@ -33,18 +31,19 @@ class Extension(): # class for handling different extension types
     def mp4(self): # handles mp4 files
         copy_files('C:/Users/David/Downloads', '.mp4')
         check = check_files('D:/Temp')
-        if check == True:
+        if check:
             remove_files('C:/Users/David/Downloads/', '.mp4')
-            return ('Copied mp4 files')
+            print('Copied mp4 files')
 
-    def mkv(self): # handles mkv files 
+    def mkv(self): # handles mkv files
         copy_files('C:/Users/David/Downloads', '.mkv')
         check = check_files('D:/Temp')
-        if check == True:
+        if check:
             remove_files('C:/Users/David/Downloads/', '.mkv')
-            return ('Copied mkv files')
+            print('Copied mkv files')
 
 
-extension = Extension() # calls the class 
+# Calls the class
+extension = Extension()
 extension.mkv()
 extension.mp4()
