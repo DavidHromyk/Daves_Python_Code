9ded53fe4fb8ca0a3fcc51c7004daf12b67388b6
David Hromyk <38985997+DavidHromyk@users.noreply.github.com>
2020-04-22 15:56:57 -0700

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -14,64 +14,68 @@ def check_win(choice,computer_choice):
         print ('\nYou Win!')
 
         
-while True:
-    
-    
-    play = input('Would you like to play Rock, Paper, Scissors?: ')
 
-    
-    if play.lower()[0] == 'y':
-        
-        clear_output()
-        print("------------------------------")
-        print("Welcome to Rock Paper Scissors")
-        print("------------------------------")
-        
-        print("\nPlease Select\n1. Rock \n2. Paper\n3. Scissors")
-        
-        try:
-            choice = int(input("Please select rock, paper, or scissors: "))
-            
+def main():
+    while True:
 
-        except:
+
+        play = input('Would you like to play Rock, Paper, Scissors?: ')
+
+
+        if play.lower()[0] == 'y':
+
             clear_output()
-            choice = int(input("Try again. Please select rock, paper, or scissors: "))
-         
-        computer_choice = random.choice(hands)
-        
-        if choice == 1:
-            choice_name = "rock"
-        elif choice == 2 :
-            choice_name = "paper"
-        else:
-            choice_name = "scissors"
-        
-        
-        if computer_choice == 1:
-            computer_choice_name = "rock"
-        elif computer_choice == 2:
-            computer_choice_name = "paper"
-        else:
-            computer_choice_name = "scissors"
-            
-        clear_output()
-        print(f"Your choice is {choice_name}")
-        print(f"\nComputer choice is {computer_choice_name}")
-        
-        # Check winning conditions
-        
-        check_win(choice,computer_choice)
-            
-        again = input("Would you like to play again?: ")
-        
-        if again.lower()[0] == 'y':
-            continue
+            print("------------------------------")
+            print("Welcome to Rock Paper Scissors")
+            print("------------------------------")
+
+            print("\nPlease Select\n1. Rock \n2. Paper\n3. Scissors")
+
+            try:
+                choice = int(input("Please select rock, paper, or scissors: "))
+
+
+            except:
+                clear_output()
+                choice = int(input("Try again. Please select rock, paper, or scissors: "))
+
+            computer_choice = random.choice(hands)
+
+            if choice == 1:
+                choice_name = "rock"
+            elif choice == 2 :
+                choice_name = "paper"
+            else:
+                choice_name = "scissors"
+
+
+            if computer_choice == 1:
+                computer_choice_name = "rock"
+            elif computer_choice == 2:
+                computer_choice_name = "paper"
+            else:
+                computer_choice_name = "scissors"
+
+            clear_output()
+            print(f"Your choice is {choice_name}")
+            print(f"\nComputer choice is {computer_choice_name}")
+
+            # Check winning conditions
+
+            check_win(choice,computer_choice)
+
+            again = input("Would you like to play again?: ")
+
+            if again.lower()[0] == 'y':
+                continue
+            else:
+                clear_output()
+                print("We'll see you next time!")
+                break 
+
         else:
             clear_output()
-            print("We'll see you next time!")
-            break 
-            
-    else:
-        clear_output()
-        print("Maybe next time")
-        break
+            print("Maybe next time")
+            break
+
+main()
