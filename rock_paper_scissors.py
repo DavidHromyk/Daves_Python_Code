40661544be71434bbc6db59efdaf4ebad6c86411
David Hromyk <38985997+DavidHromyk@users.noreply.github.com>
2020-04-23 10:42:11 -0700

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,5 @@
 import random
-from IPython.display import clear_output
+import os
 import sys
 
 player_wins = 0
@@ -7,27 +7,28 @@ computer_wins = 0
 
 
 def check_win(choice,computer_choice):
-    
+    global computer_wins
+    global player_wins
     if choice == 1 and computer_choice == 1 or choice == 2 and computer_choice == 2 or choice == 3 and computer_choice ==3:
         print ('\nDraw!')
         print("_____________________________")
     if choice == 1 and computer_choice == 2 or choice == 3 and computer_choice == 1 or choice == 2 and computer_choice ==3:
         print ('\nComputer Wins!')
         print("_____________________________")
-        global computer_wins
         computer_wins = computer_wins + 1
         print(f"\nComputer wins {computer_wins}")
+        print(f"\nPlayer 1 wins {player_wins}")
     if choice == 1 and computer_choice == 3 or choice == 2 and computer_choice == 1 or choice == 3 and computer_choice ==2:
         print ('\nYou Win!')
         print("_____________________________")
-        global player_wins
         player_wins = player_wins + 1
         print(f"\nPlayer 1 wins {player_wins}")
+        print(f"\nComputer wins {computer_wins}")
     
     if computer_wins == 3:
-        sys.exit("Computer has won it all!")
+        sys.exit("Computer has won it all!\n")
     if player_wins == 3:
-        sys.exit("Player 1 has won it all!")
+        sys.exit("Player 1 has won it all!\n")
 
 def play_again():
     return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
@@ -39,11 +40,11 @@ def playing():
     print("\nPlease Select\n1. Rock \n2. Paper\n3. Scissors")
 
     try:
-        choice = int(input("Please select rock, paper, or scissors: "))
+        choice = int(input("\nPlease select rock, paper, or scissors: "))
 
 
     except:
-        clear_output()
+        os.system('cls')
         choice = int(input("Try again. Please Select\n\n1. Rock \n2. Paper\n3. Scissors: "))
 
             
@@ -63,7 +64,7 @@ def playing():
     else:
         computer_choice_name = "scissors"
 
-    clear_output()
+    os.system('cls')
     print("_____________________________")
     print(f"\nYour choice is {choice_name}")
     print(f"\nComputer choice is {computer_choice_name}")
@@ -74,10 +75,10 @@ def playing():
 def main():       
     
     while True:
-        play = input('Would you like to play Rock, Paper, Scissors?: ')
+        play = input('\nWould you like to play Rock, Paper, Scissors?: ')
         
         if play.lower()[0] == 'y':
-            clear_output()
+            os.system('cls')
             print("------------------------------")
             print("Welcome to Rock Paper Scissors")
             print("------------------------------")
@@ -91,12 +92,7 @@ def main():
             playing()
             
         else:
-            clear_output()
+            os.system('cls')
             print("Maybe we'll play next time")
-            break
-        
-        if not play_again():
-            clear_output()
-            print('Maybe another time')
             break
 main()
